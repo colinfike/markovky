cf69a725571c7881dd50e544572849976fde3e29
Colin Fike <colin.fike@gmail.com>
2016-12-14 23:08:17 -0500

    I did not debug this for an hour to have this be the issue, right?

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-set :rails_env, 'staging'
+set :rails_env, 'production'
 server '52.91.231.182', user: 'ubuntu', roles: %w{app db web}
 
 # role-based syntax
