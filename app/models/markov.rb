10890a4851a27064a33f8ecfc801b2dcfe53e5f9
Colin Fike <colin.fike@gmail.com>
2016-12-16 22:58:40 -0500

    Lite code updates

--- app/models/markov.rb
+++ app/models/markov.rb
@@ -17,7 +17,6 @@ class Markov < ApplicationRecord
         last_id = tweet['id'] - 1
         posts << tweet['text']
       end
-      logger.info posts
       posts.each do |post|
         temporary_markov_hash = self.process_post(temporary_markov_hash, post)
       end
@@ -27,42 +26,41 @@ class Markov < ApplicationRecord
     user.update(markov_chain: temporary_markov_hash)
   end
 
-# # Strips out garbage text and iterates over each word
-def self.process_post temporary_markov_hash, post
-  past_word = '['
-  current_word = '['
-  # Remove links then tear everything apart at terminal points
-  post.gsub(/http\S+/, '').split('.!?').each_with_index do |sentence, index|
-    # Converts html entity names back to their actual ASCII values, removes returns and all non-alphanumeric values (and spaces)
-    word_array = CGI::unescapeHTML(sentence).gsub(/\n|[^A-Za-z@# ]|@\w+/,'').split(' ')
-    # word_array = CGI::unescapeHTML(sentence).gsub(/\n/, "").gsub(/[^A-Za-z@# ]/, '').gsub(/@\w+/).split(' ')
-    word_count = word_array.count
+  # # Strips out garbage text and iterates over each word
+  def self.process_post temporary_markov_hash, post
+    past_word = '['
+    current_word = '['
+    # Remove links then tear everything apart at terminal points
+    post.gsub(/http\S+/, '').split('.!?').each_with_index do |sentence, index|
+      # Converts html entity names back to their actual ASCII values, removes returns and all non-alphanumeric values (and spaces)
+      word_array = CGI::unescapeHTML(sentence).gsub(/\n|[^A-Za-z@# ]|@\w+/,'').split(' ')
+      # word_array = CGI::unescapeHTML(sentence).gsub(/\n/, "").gsub(/[^A-Za-z@# ]/, '').gsub(/@\w+/).split(' ')
+      word_count = word_array.count
 
-    word_array.each_with_index do |word, current_word_index|
-      past_word = current_word
-      current_word = word
-      # Increment the count of the current_word key's value in past_word hash
-      temporary_markov_hash[past_word] = {} if !temporary_markov_hash[past_word]
-      temporary_markov_hash[past_word][current_word] = temporary_markov_hash[past_word][current_word].to_i + 1
+      word_array.each_with_index do |word, current_word_index|
+        past_word = current_word
+        current_word = word
+        # Increment the count of the current_word key's value in past_word hash
+        temporary_markov_hash[past_word] = {} if !temporary_markov_hash[past_word]
+        temporary_markov_hash[past_word][current_word] = temporary_markov_hash[past_word][current_word].to_i + 1
 
-      # If this is the last word in the sentence then take the current word and increment the BOL key's value.
-      temporary_markov_hash[current_word] = {} if !temporary_markov_hash[current_word]
-      temporary_markov_hash[current_word][']'] = temporary_markov_hash[current_word][']'].to_i + 1 if current_word_index == word_count - 1
+        # If this is the last word in the sentence then take the current word and increment the BOL key's value.
+        temporary_markov_hash[current_word] = {} if !temporary_markov_hash[current_word]
+        temporary_markov_hash[current_word][']'] = temporary_markov_hash[current_word][']'].to_i + 1 if current_word_index == word_count - 1
+      end
     end
+    return temporary_markov_hash
   end
-  return temporary_markov_hash
-end
 
-def self.generate_sentence(user)
-  chosen_word = "["
-  sentence = ""
-  while chosen_word != "]"
-    word_hash = user.markov_chain[chosen_word]
-    randomizer = WeightedRandomizer.new(word_hash)
-    chosen_word = randomizer.sample
-    sentence << chosen_word + " " if chosen_word != "]"
+  def self.generate_sentence(user)
+    chosen_word = "["
+    sentence = ""
+    while chosen_word != "]"
+      word_hash = user.markov_chain[chosen_word]
+      randomizer = WeightedRandomizer.new(word_hash)
+      chosen_word = randomizer.sample
+      sentence << chosen_word + " " if chosen_word != "]"
+    end
+    return sentence.downcase.capitalize.chomp(' ') + "."
   end
-  return sentence.downcase.capitalize.chomp(' ') + "."
-end
-# end
 end
