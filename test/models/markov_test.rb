094fbdbcfd8952566557e1c9622a6639bfd5f80f
Colin Fike <colin.fike@gmail.com>
2016-12-18 23:14:40 -0500

    Long weekend but lets at least get another test in here:

--- test/models/markov_test.rb
+++ test/models/markov_test.rb
@@ -4,4 +4,10 @@ class MarkovTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "valid twitter user generates a dictionary" do
+    user = User.create(twitter_username: "colinfike")
+    Markov.create_twitter_markov_chain(user)
+    assert user.markov_chain
+  end
 end
