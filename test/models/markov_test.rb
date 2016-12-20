a74c3b8f9ead0727e794170cf2fea7e854af0b62
Colin Fike <colin.fike@gmail.com>
2016-12-19 23:00:53 -0500

    Add a bit more test coverage, I think this makes 100% test coverage of the models

--- test/models/markov_test.rb
+++ test/models/markov_test.rb
@@ -1,13 +1,26 @@
 require 'test_helper'
 
 class MarkovTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   test "valid twitter user generates a dictionary" do
     user = User.create(twitter_username: "colinfike")
     Markov.create_twitter_markov_chain(user)
     assert user.markov_chain
+  end
+
+  test "bearer token as still valid" do
+    response = RestClient.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=colinfike", {"Authorization" => "Bearer #{TWITTER_BEARER_TOKEN}" })
+    assert response
+  end
+
+  test "post processing" do
+    temp_markov = {}
+    temp_markov = Markov.process_post(temp_markov, "@colinfike Hey!! \n funny stuff http://tester.com/whop")
+    assert temp_markov == {"["=>{"Hey"=>1}, "Hey"=>{"funny"=>1}, "funny"=>{"stuff"=>1}, "stuff"=>{"]"=>1}}
+  end
+
+  test "sentence generation" do
+    user = User.create(twitter_username: 'colinfike', markov_chain: {"["=>{"Hey"=>1}, "Hey"=>{"funny"=>1}, "funny"=>{"stuff"=>1}, "stuff"=>{"]"=>1}})
+    generated_sentence = Markov.generate_sentence(user)
+    assert generated_sentence == "Hey funny stuff."
   end
 end
