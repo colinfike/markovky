6b45de6261b86c68468a8d006d78617b67ec94d1
Colin Fike <colin.fike@gmail.com>
2016-12-22 23:28:11 -0500

    Add some fixtures to clean up testing

--- test/models/markov_test.rb
+++ test/models/markov_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MarkovTest < ActiveSupport::TestCase
   test "valid twitter user generates a dictionary" do
-    user = User.create(twitter_username: "colinfike")
+    user = users(:colin)
     Markov.create_twitter_markov_chain(user)
     assert user.markov_chain
   end
@@ -19,13 +19,13 @@ class MarkovTest < ActiveSupport::TestCase
   end
 
   test "sentence generation" do
-    user = User.create(twitter_username: 'colinfike', markov_chain: {"["=>{"Hey"=>1}, "Hey"=>{"funny"=>1}, "funny"=>{"stuff"=>1}, "stuff"=>{"]"=>1}})
+    user = users(:colin_with_markov)
     generated_sentence = Markov.generate_sentence(user)
     assert_equal generated_sentence, "Hey funny stuff."
   end
 
   test "sentence generation graceful failure" do
-    user = User.create(twitter_username: 'colinfike')
+    user = users(:colin)
     generated_sentence = Markov.generate_sentence(user)
     assert_not generated_sentence
   end
